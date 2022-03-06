35fc1641f2b16da20fbad7f996414292f83ab690
Ndubuisi Eze <ndubuisieze60@gmail.com>
2022-03-06 20:37:07 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
       <Routes>
-        <Route exact path="/eureka-demo/home" element={<Home />} />
+        <Route exact path="/eureka-demo" element={<Home />} />
       </Routes>
     </BrowserRouter>
     </div>
